877adf3e4ec404c51eb0b2a7d909fbd708132b62
pejerx <jerspooh@gmail.com>
2026-07-08 03:28:28 +0800

    Update: Created the Item Report Repo to initiate requests

---
+++ backend/src/main/java/edu/cit/garol/campus/repository/ItemReportRepository.java
@@ -0,0 +1,7 @@
+package edu.cit.garol.campus.repository;
+
+import edu.cit.garol.campus.entity.ItemReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemReportRepository extends JpaRepository<ItemReport, Long> {
+}
